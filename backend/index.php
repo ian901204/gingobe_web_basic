0caa53450dc595297e413be6de160b9a2f0ac762
ian901204 <scckate444@gmail.com>
2022-09-15 22:24:47 +0800

    feat:backend index.php slim

---
+++ backend/index.php
@@ -0,0 +1,13 @@
+<?php
+use Slim\Factory\AppFactory;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+$app->setBasePath(__DIR__);
+$app->get('', function ($request, $response, array $args) {
+	return $response->getBody() -> write("hi");
+	 });
+
+$app->run();
