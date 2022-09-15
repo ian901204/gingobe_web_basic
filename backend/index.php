f611d0206ef9042bc3e5cdba8388cadf8b9423ff
scckate444 <scckate444@gmail.com>
2022-09-15 23:23:30 +0800

    feat: updaye

--- backend/index.php
+++ backend/index.php
@@ -6,7 +6,7 @@ require __DIR__.'/../vendor/autoload.php';
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($request, $response, array $args) {
-	$response->getBody() -> write(file_get_contents("order.php"));
+	$response->getBody() -> write(include "order.php");
 	return $response -> withStatus(200);
 });
 
