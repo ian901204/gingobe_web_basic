d64c9afd1226695848aa6ed9aed736c0d32017ef
scckate444 <scckate444@gmail.com>
2022-09-15 22:33:08 +0800

    test: index.php return other page

--- backend/index.php
+++ backend/index.php
@@ -6,7 +6,7 @@ require __DIR__.'/../vendor/autoload.php';
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($request, $response, array $args) {
-	$response->getBody() -> write("hi");
+	$response->getBody() -> write(file_get_contents("order.php"));
 	return $response -> withStatus(200);
 });
 
