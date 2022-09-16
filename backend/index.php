362bd16b6d2b022e458803fdd452b8d2ae9481a1
ian901204 <scckate444@gmail.com>
2022-09-16 13:43:16 +0800

    feat: route add order

--- backend/index.php
+++ backend/index.php
@@ -6,7 +6,11 @@ require __DIR__.'/../vendor/autoload.php';
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($request, $response, array $args) {
-	$response->getBody() -> write(include "order.php");
+	$response->getBody() -> write(include "blank.php");
+	return $response -> withStatus(200);
+});
+$app -> get("/order", function ($request, $response){
+	$response -> getBody() -> write(include "order.php");
 	return $response -> withStatus(200);
 });
 
