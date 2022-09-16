4607c8fb4d84f25129d1f163c8fcae80c64bdb39
ian901204 <scckate444@gmail.com>
2022-09-16 14:08:33 +0800

    feat

--- backend/index.php
+++ backend/index.php
@@ -5,6 +5,17 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 $app -> get('/', function ($request, $response, array $args) {
 	$response->getBody() -> write(include "blank.php");
 	return $response -> withStatus(200);
