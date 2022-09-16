fba87472c05dd4cdeaa97fb47156ecc8c37c4684
ian901204 <scckate444@gmail.com>
2022-09-16 14:16:33 +0800

    test: index CORS testing

--- backend/index.php
+++ backend/index.php
@@ -5,17 +5,27 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
-$app->options('/{routes:.+}', function ($request, $response, $args) {
+// This middleware will append the response header Access-Control-Allow-Methods with all allowed methods
+$app->add(function (Request $request, RequestHandlerInterface $handler): Response {
+    $routeContext = RouteContext::fromRequest($request);
+    $routingResults = $routeContext->getRoutingResults();
+    $methods = $routingResults->getAllowedMethods();
+    $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
+    $response = $handler->handle($request);
+
+    $response = $response->withHeader('Access-Control-Allow-Origin', 'https://test');
+    $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+    $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+    // Optional: Allow Ajax CORS requests with Authorization header
+    // $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+
     return $response;
 });
 
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
+// The RoutingMiddleware should be added after our CORS middleware so routing is performed first
+$app->addRoutingMiddleware();
 $app -> get('/', function ($request, $response, array $args) {
 	$response->getBody() -> write(include "blank.php");
 	return $response -> withStatus(200);
