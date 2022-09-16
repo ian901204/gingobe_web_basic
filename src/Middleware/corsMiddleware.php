c7212b5771d5da26c3a1945006e432905a6c5720
ian901204 <scckate444@gmail.com>
2022-09-16 14:35:03 +0800

    feat: add corsMiddleware

---
+++ src/Middleware/corsMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class corsMiddleware
+{
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', '*')
+            ->withHeader('Access-Control-Allow-Credentials', "true")
+            ->withHeader('Access-Control-Allow-Methods', '*');
+    }
+
+}
