b0d6ba91e6be9f1d3ef4ba04be2a19d76f74e6fa
ian901204 <scckate444@gmail.com>
2022-09-16 13:41:00 +0800

    feat: menu update

--- backend/menu.php
+++ backend/menu.php
@@ -3,7 +3,7 @@
 <div id="main-menu" class="main-menu collapse navbar-collapse">
     <ul class="nav navbar-nav">
         <li>
-            <a href="order.php"><i class="menu-icon ti-email"></i>訂單列表</a>
+            <a href="/order"><i class="menu-icon ti-email"></i>訂單列表</a>
         </li>
     </ul>
 </div><!-- /.navbar-collapse -->
