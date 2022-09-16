121f607de550aa40b022926c482a531e05396354
ian901204 <scckate444@gmail.com>
2022-09-16 13:44:49 +0800

    feat: update index route

--- backend/index.php
+++ backend/index.php
@@ -9,8 +9,8 @@ $app->get('/', function ($request, $response, array $args) {
 	$response->getBody() -> write(include "blank.php");
 	return $response -> withStatus(200);
 });
-$app -> get("/order", function ($request, $response){
-	$response -> getBody() -> write(include "order.php");
+$app -> get('/order', function ($request, $response) {
+	$response->getBody() -> write(include "order.php");
 	return $response -> withStatus(200);
 });
 
