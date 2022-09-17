b6fa774e94fe281418a4561636ac99f3901cae73
scckate444 <scckate444@gmail.com>
2022-09-17 20:23:25 +0800

    update position

--- public/detail.php
+++ public/detail.php
@@ -120,37 +120,37 @@
                                     <td scope="row" colspan = "2">可以回收再利用.</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">焚化</th>
+                                    <th scope="row" colspan = "2">焚化</th>
                                     <td scope="row" colspan = "2">容易黏在在爐壁上，不易清潔</td>
                                     <td scope="row" colspan = "2">不會粘在爐壁上</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">降解時間</th>
+                                    <th scope="row" colspan = "2">降解時間</th>
                                     <td scope="row" colspan = "2">很難改變</td>
                                     <td scope="row" colspan = "2">可根據產品要求調整</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">生物降解方式</th>
+                                    <th scope="row" colspan = "2">生物降解方式</th>
                                     <td scope="row" colspan = "2">工業堆肥, 溫度58C, 在有氧環境下</td>
                                     <td scope="row" colspan = "2">在大自然露天環境4-6月風化,  <br>風化後在730天內,礦化為CO2,  水及其他生質</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">優, 劣勢分析(一)</th>
+                                    <th scope="row" colspan = "2">優, 劣勢分析(一)</th>
                                     <td scope="row" colspan = "2">PLA沒有專門之回收途徑. <br>常丟到一般塑料回收桶中,嚴重影呴正常回收料之品質.</td>
                                     <td scope="row" colspan = "2">回收途徑比較健全</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">優, 劣勢分析(二)</th>
+                                    <th scope="row" colspan = "2">優, 劣勢分析(二)</th>
                                     <td scope="row" colspan = "2">各國沒有足夠之工業堆肥設施, PLA 只能掩埋或焚化.</td>
                                     <td scope="row" colspan = "2">傳統塑料+BADP-Icycle Pro,大自然露天環境下, 常溫可風化及生物降解.</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">優, 劣勢分析(三)</th>
+                                    <th scope="row" colspan = "2">優, 劣勢分析(三)</th>
                                     <td scope="row" colspan = "2">在歐美國家, 生質塑料聲量大, 標準都是由生質塑料及工業堆肥業者訂定. 規定在180天, 分解率要90%. 目的是排除傳統塑料業者. PLA 是生質塑料的龍頭, 己經被環保人士認同,為PLA 之優勢. 實際上PLA 在大自然環境中, 常溫下,180天是很難分解完成.</td>
                                     <td scope="row" colspan = "2">傳統塑料+BADP-Icycle Pro, 降解速度不如 PLA快, 比較緩慢. 生質塑料及工業堆肥業者, 為保障其利益, 容易被人以此點質疑我們, 說傳統塑料是無法降解.</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">優, 劣勢分析(四)</th>
+                                    <th scope="row" colspan = "2">優, 劣勢分析(四)</th>
                                     <td scope="row" colspan = "2">PLA 分解速率比傳統塑料快, 是其優勢, 但保質期短是劣勢.</td>
                                     <td scope="row" colspan = "2">傳統塑料+BADP-Icycle Pro, 降解速度比較緩慢. 但不是不會降解. 保質期長是其優勢.</td>
                                 </tr>
