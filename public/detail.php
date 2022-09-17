fe496741549f438361381b559c59e99a25760851
scckate444 <scckate444@gmail.com>
2022-09-17 16:33:41 +0800

    feat: detail page update

--- public/detail.php
+++ public/detail.php
@@ -143,48 +143,6 @@
 
   </div>
 </div>
-
-<div class="py-vh-6 bg-gray-900 text-light w-100 overflow-hidden" id="order">
-  <div class="container">
-    <div class="row d-flex justify-content-center">
-      <div class="row d-flex justify-content-center text-center">
-  <div class="col-lg-8 text-center" data-aos="fade">
-  <div class="row">
-    <div class="col center">
-      <form class="form-floating">
-        <div class="col">
-          <h3 class="h5 my-2">請輸入您的大名</h3>
-          <input data-aos="fade-down" type="text" class="form-control p-3" id="name" placeholder="請輸入姓名">
-        </div>
-        <br>
-        <div class = "col">
-          <input data-aos="fade-down" type="text" class="form-control p-3" id="address" placeholder="請輸入地址">
-        </div>
-        <div class = 'col'>
-          <input data-aos="fade-down" type="text" class="form-control p-3" id="phone" placeholder="09XXXXXXXX">
-        </div>
-        <div class = 'col'>
-        </div>
-        <div class = 'col'>
-        <select data-aos="fade-down" class = "form-control p-3" id = "seller" placeholder = "請選擇業務姓名">
-          <option value = 0>業務一</option>
-          <option value = 1>業務二</option>
-        </select>
-        </div>
-      
-    </div>
-  </div>
-  </div>
-  <div class="col-12">
-    <a href="#" class="btn btn-warning btn-xl shadow me-3 mt-4" data-aos="fade-down">訂購</a>
-  </div>
-  </form>
-</div>
-
-    </div>
-  </div>
-</div>
-
     </main>
 
     <footer>
@@ -192,17 +150,9 @@
     <div class="row py-5 d-flex justify-content-between">
 
 <div class="col-12 text-center">
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-layers-half" viewbox="0 0 16 16">
-    <path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/>
-  </svg>
-  <address class="text-secondary mt-3">
-    <strong>金勾杯股份有限公司</strong><br>
-    臺中市南屯區溝墘里大業路241號3樓之1<br>
-    <abbr title="Phone">P:</abbr>
-    0972-742-963
-    <abbr title="email">Email:</abbr>
-    domo58816@gmail.com
-  </address>
+  <?php
+    include "footer.php";
+  ?>
 </div>
 
 </div>
---
+++ public/footer.php
@@ -0,0 +1,11 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-layers-half" viewbox="0 0 16 16">
+    <path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/>
+  </svg>
+  <address class="text-secondary mt-3">
+    <strong>金勾杯股份有限公司</strong><br>
+    臺中市南屯區溝墘里大業路241號3樓之1<br>
+    <abbr title="Phone">P:</abbr>
+    0972-742-963
+    <abbr title="email">Email:</abbr>
+    domo58816@gmail.com
+  </address>
