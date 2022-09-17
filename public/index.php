a651baed9114e2e195530d11e3ded7fe59a3a49d
scckate444 <scckate444@gmail.com>
2022-09-17 15:12:19 +0800

    fix: phone web display menu error

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
   <meta name="author" content="Holger Koenemann">
   <meta name="generator" content="Eleventy v2.0.0">
   <meta name="HandheldFriendly" content="true">
-  <title>產品介紹</title>
+  <title>金勾杯股份有限公司</title>
   <link rel="stylesheet" href="css/theme.min.css">
 
 <style>
@@ -65,16 +65,15 @@
   <div class="container">
 
   <!--web icon positioin-->
-   <!-- <a class="navbar-brand pe-4 fs-3 border-end" href="/">
+   <a class="navbar-brand pe-4 fs-3 border-end" href="/">
   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-layers-half" viewbox="0 0 16 16">
     <path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/>
   </svg>
-  <span class="ms-1 fw-bold">Stride</span>
+  <span class="ms-1 fw-bold">金勾杯股份有限公司</span>
 </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--->
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
