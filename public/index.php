6fd28e56137c9f1b21d9000ea4cb4d3ab7017a96
scckate444 <scckate444@gmail.com>
2022-09-18 14:35:02 +0800

    update

--- public/index.php
+++ public/index.php
@@ -60,60 +60,9 @@
   </head>
 
   <body data-bs-spy="scroll" data-bs-target="#navScroll">
-
-    <nav id="navScroll" class="navbar navbar-expand-lg navbar-light fixed-top" tabindex="0">
-  <div class="container">
-
-  <!--web icon positioin-->
-   <a class="navbar-brand pe-4 fs-3 border-end" href="/">
-  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-layers-half" viewbox="0 0 16 16">
-    <path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/>
-  </svg>
-  <span class="ms-1 fw-bold">金勾杯股份有限公司</span>
-</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
-    <a class="nav-link" href="#services" aria-label="Brings you to the frontpage">
-      服務
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#aboutus">
-      關於我們
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#gallery">
-      介紹
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#order">
-      訂購
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#testimonials">
-      聯絡我們
-    </a>
-  </li>
-
-    </ul>
-    <!-- 原 icon -->
-      <!--<a href="#" aria-label="Link to the customers profil page" class="link-dark bg-white-95 p-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person" viewbox="0 0 16 16">
-          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-        </svg>
-        <span class="d-none">Profil</span>
-      </a>-->
-    </div>
-</div>
-</nav>
-
+    <?php
+      include "menu.php";
+    ?>
     <main>
       <div class="w-100 overflow-hidden bg-gray-100" id="top">
   
@@ -248,22 +197,21 @@
 <div class="container py-vh-4 w-100 overflow-hidden">
   <div class="row d-flex justify-content-center align-items-center">
     <div class="col-lg-5">
-  <h3 class="py-5 border-top border-dark" data-aos="fade-right">關於我們</h3>
-</div>
-<div class="col-md-7" data-aos="fade-left">
-  <blockquote>
-    <div class="fs-4 my-3 fw-light pt-4 border-bottom pb-3">本公司（金勾盃股份有限公司）為皇盃國際塑膠廠的總代理商，
+      <h3 class="py-5 border-top border-dark" data-aos="fade-right">關於我們</h3>
+    </div>
+    <div class="col-md-7" data-aos="fade-left">
+      <blockquote>
+        <div class="fs-4 my-3 fw-light pt-4 border-bottom pb-3">本公司（金勾盃股份有限公司）為皇盃國際塑膠廠的總代理商，
 雖然本公司上市未達１年，但本公司保持著以客至上、客為優先的態度在營運，所以客戶可以完全的放心交給我們！
 我們的塑膠環保杯為前所未見得新開發產品，
 專利擁有者在２０２１年的某一天，在等紅燈的途中，看了一下飲料店擁有美貌的飲料店服務員，正在欣賞的途中，突然看到了正在買飲料的客人買了袋子準備裝飲料，頓時腦海中浮出了「為何不在飲料杯上多做一個提環，店家、客人都方便且還環保！！」於是專利擁有者開始動身，經過了許多挫折、壓力、測試、意見不合等，
 於是，燈愣～「金勾杯」就誕生了。
-</div>
+        </div>
     <!--<img src="img/person11.jpg" width="64" height="64" class="img-fluid rounded-circle me-3" alt="" data-aos="fade">
     <span><span class="fw-bold">John Doe,</span>
       CEO of Stride Ltd.</span>-->
-  </blockquote>
-</div>
-
+      </blockquote>
+    </div>
   </div>
 </div>
 
@@ -271,56 +219,54 @@
   <div class="container">
     <div class="row d-flex justify-content-center">
       <div class="row d-flex justify-content-center text-center">
-  <div class="col-lg-8 text-center" data-aos="fade">
-  <div class="row">
-    <div class="col center">
-      <form class="form-floating">
-        <div class="col">
-          <h3 class="h5 my-2" data-aos="fade-left">請輸入您的大名</h3>
-          <input data-aos="fade-left" type="text" class="form-control p-3" id="name" placeholder="請輸入姓名">
+        <div class="col-lg-8 text-center" data-aos="fade">
+          <div class="row">
+            <div class="col center">
+              <form class="form-floating">
+              <div class="col">
+                <h3 class="h5 my-2" data-aos="fade-left">請輸入您的大名</h3>
+                <input data-aos="fade-left" type="text" class="form-control p-3" id="name" placeholder="請輸入姓名">
+              </div>
+              <br>
+              <div class = "col">
+              <h3 class="h5 my-2" data-aos="fade-left">請輸入您的地址</h3>
+                <input data-aos="fade-right" type="text" class="form-control p-3" id="address" placeholder="請輸入地址">
+              </div>
+              <br>
+              <div class = 'col'>
+              <h3 class="h5 my-2" data-aos="fade-left">請輸入您的電話</h3>
+                <input data-aos="fade-left" type="text" class="form-control p-3" id="phone" placeholder="09XXXXXXXX">
+              </div>
+              <br>
+              <div class = 'col'>
+              <h3 class="h5 my-2" data-aos="fade-left">請輸入欲訂購的尺寸</h3>
+              <select data-aos="fade-right" class = "form-control p-3" id = "size" placeholder = "請選擇尺寸">
+                <option value = 700>PP700</option>
+                <option value = 660>PP660</option>
+                <option value = 500>PP500</option>
+              </select>
+              </div>
+              <br>
+              <div class = 'col'>
+              <h3 class="h5 my-2" data-aos="fade-left">請輸入欲訂購的箱數(1000杯/箱)</h3>
+                <input data-aos="fade-left" type="number" class="form-control p-3" id="amount" placeholder="箱數">
+              </div>
+              <br>
+              <div class = 'col'>
+              <h3 class="h5 my-2" data-aos="fade-left">請選擇業務員名稱</h3>
+              <select data-aos="fade-right" class = "form-control p-3" id = "seller" placeholder = "請選擇業務姓名">
+                <option value = 0>業務一</option>
+                <option value = 1>業務二</option>
+              </select>
+              </div>
+              <div class="col-12">
+                <a href="#" class="btn btn-warning btn-xl shadow me-3 mt-4" data-aos="fade-up">訂購</a>
+              </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <br>
-        <div class = "col">
-        <h3 class="h5 my-2" data-aos="fade-left">請輸入您的地址</h3>
-          <input data-aos="fade-right" type="text" class="form-control p-3" id="address" placeholder="請輸入地址">
-        </div>
-        <br>
-        <div class = 'col'>
-        <h3 class="h5 my-2" data-aos="fade-left">請輸入您的電話</h3>
-          <input data-aos="fade-left" type="text" class="form-control p-3" id="phone" placeholder="09XXXXXXXX">
-        </div>
-        <br>
-        <div class = 'col'>
-        <h3 class="h5 my-2" data-aos="fade-left">請輸入欲訂購的尺寸</h3>
-        <select data-aos="fade-right" class = "form-control p-3" id = "size" placeholder = "請選擇尺寸">
-          <option value = 700>PP700</option>
-          <option value = 660>PP660</option>
-          <option value = 500>PP500</option>
-        </select>
-        </div>
-        <br>
-        <div class = 'col'>
-        <h3 class="h5 my-2" data-aos="fade-left">請輸入欲訂購的箱數(1000杯/箱)</h3>
-          <input data-aos="fade-left" type="number" class="form-control p-3" id="amount" placeholder="箱數">
-        </div>
-        <br>
-        <div class = 'col'>
-        <h3 class="h5 my-2" data-aos="fade-left">請選擇業務員名稱</h3>
-        <select data-aos="fade-right" class = "form-control p-3" id = "seller" placeholder = "請選擇業務姓名">
-          <option value = 0>業務一</option>
-          <option value = 1>業務二</option>
-        </select>
-        </div>
-      
-    </div>
-  </div>
-  </div>
-  <div class="col-12">
-    <a href="#" class="btn btn-warning btn-xl shadow me-3 mt-4" data-aos="fade-up">訂購</a>
-  </div>
-  </form>
-</div>
-
+      </div>
     </div>
   </div>
 </div>
@@ -373,7 +319,6 @@
     </div>
   </div>
 </div>-->
-
 </main>
 
 <footer>
