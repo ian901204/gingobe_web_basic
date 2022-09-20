ed17fbd4dc19de3f1b620578017327659c411609
ian901204 <scckate444@gmail.com>
2022-09-20 15:02:15 +0800

    feat: front end change to slim framework

--- backend/index.php
+++ backend/index.php
@@ -10,27 +10,15 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
-// This middleware will append the response header Access-Control-Allow-Methods with all allowed methods
-$app->add(function (Request $request, RequestHandlerInterface $handler): Response {
-    $routeContext = RouteContext::fromRequest($request);
-    $routingResults = $routeContext->getRoutingResults();
-    $methods = $routingResults->getAllowedMethods();
-    $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
 
-    $response = $handler->handle($request);
-
-    $response = $response->withHeader('Access-Control-Allow-Origin', 'https://test');
-    $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
-    $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
-
-    // Optional: Allow Ajax CORS requests with Authorization header
-    // $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
-
+// CORS
+$app->options("/{routes:.+}", function (ServerRequestInterface $request, ResponseInterface $response) {
+    $response->getBody()->write("OK");
     return $response;
 });
 
-// The RoutingMiddleware should be added after our CORS middleware so routing is performed first
-$app->addRoutingMiddleware();
+// CORS Headers
+$app->add(corsMiddleware::class);
 $app -> get('/', function ($request, $response, array $args) {
 	$response->getBody() -> write(include "blank.php");
 	return $response -> withStatus(200);
---
+++ public/main.php
@@ -0,0 +1,357 @@
+<!doctype html>
+<html class="h-100" lang="en">
+
+  <head>
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+  <meta name="description" content="A growing collection of ready to use components for the CSS framework Bootstrap 5">
+  <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon.png">
+  <meta name="author" content="Holger Koenemann">
+  <meta name="generator" content="Eleventy v2.0.0">
+  <meta name="HandheldFriendly" content="true">
+  <title>金勾杯股份有限公司</title>
+  <link rel="stylesheet" href="css/theme.css">
+
+<style>
+
+  /* inter-200 - latin */
+  @font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 200;
+  font-display: swap;
+  src: local(''),
+      url('../fonts/inter-v11-latin-200.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+      url('../fonts/inter-v11-latin-200.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+  /* inter-300 - latin */
+  @font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: local(''),
+      url('../fonts/inter-v11-latin-300.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+      url('../fonts/inter-v11-latin-300.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+  /* inter-regular - latin */
+  @font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local(''),
+      url('../fonts/inter-v11-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+      url('../fonts/inter-v11-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+  /* inter-500 - latin */
+  @font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: local(''),
+      url('../fonts/inter-v11-latin-500.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+      url('../fonts/inter-v11-latin-500.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+
+</style>
+
+  </head>
+
+<body data-bs-spy="scroll" data-bs-target="#navScroll">
+<?php
+  include "menu.php";
+?>
+<main>
+<div class="w-100 overflow-hidden bg-gray-100" id="top">
+  <div class="container position-relative">
+    <div class="col-12 col-lg-8 mt-0 h-100 position-absolute top-0 end-0 bg-cover" data-aos="fade-left" style="background-image: url(img/interior11.jpg);">
+    </div>
+    <div class="row">
+      <div class="col-lg-7 py-vh-3 position-relative" data-aos="fade-right">
+        <h1 class="display-1 fw-bold mt-5">環保塑膠杯</h1>
+        <p class="lead"> 
+          本產品一體成形、手提專利飲料杯，BADP生物可降解塑料、PP材質食品級原料可回收再生能源，愛護地球、守護海洋從不拿塑膠袋開始！
+      高透明、清晰，可以清楚看見杯子內的配料，可承受拉力為8公斤！！拿著飲料怎麼甩怎麼跑都不會斷，且耐高溫120°C，讓客人不用因為釋出塑化劑而喝下肚，有SGS材質及拉力檢驗證明，少拿一個塑膠袋，省錢又環保的好選擇！！
+        </p>
+        <a href="#order" class="btn btn-dark btn-xl shadow me-3 rounded-0 my-5">訂購</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="py-vh-3 bg-gray-200 w-100 overflow-hidden" id="services">
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-lg-10 aos-init border-bottom border-dark aos-animate" data-aos="fade-down"><h2 class="display-6">我們的產品 BADP-icycle Pro + PP 所製成的環保塑膠杯與傳統 PLA 所製成的塑膠杯相比，有以下幾種優點</h2><br>
+      </div>
+    </div>
+    <br>
+    <div class="row d-flex align-items-center">
+      <?php
+        include "detail.php";
+      ?>
+    </div>
+    <br>
+  </div>
+</div>
+
+<div class="py-vh-4 bg-gray-100 w-100 overflow-hidden" id="aboutus">
+  <div class="container">
+      
+<div class="row d-flex justify-content-between align-items-center">
+  <div class="col-lg-6">
+  <div class="row gx-5 d-flex">
+    <div class="col-md-11">
+      <div class="shadow ratio ratio-16x9 rounded bg-cover bp-center align-self-end" data-aos="fade-right" style="background-image: url(img/people15.jpg);--bs-aspect-ratio: 50%;">
+      </div>
+    </div>
+    <div class="col-md-5 offset-md-1">
+      <div class="shadow ratio ratio-1x1 rounded bg-cover mt-5 bp-center float-end" data-aos="fade-up" style="background-image: url(img/interior42.jpg);">
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="col-12 shadow ratio rounded bg-cover mt-5 bp-center" data-aos="fade-left" style="background-image: url(img/people4.jpg);--bs-aspect-ratio: 150%;">
+      </div>
+    </div>
+  </div>
+</div>
+<div class="col-lg-4">
+  <h3 class="py-5 border-top border-dark" data-aos="fade-left">We did some interesting stuff in our field of work. For example we collect a lot of these free photos and use them on our website.</h3>
+  <p data-aos="fade-left" data-aos-delay="200">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+  </p>
+  <p>
+    <a href="#" class="link-fancy link-dark" data-aos="fade-left" data-aos-delay="400">Learn more
+    </a>
+  </p>
+</div>
+</div>
+
+  </div>
+</div>
+
+<div class="py-vh-5 w-100 overflow-hidden" id="numbers">
+  <div class="container">
+      <div class="row d-flex justify-content-between align-items-center">
+  <div class="col-lg-5">
+    <h3 class="py-5 border-top border-dark" data-aos="fade-right">Our magic numbers</h3>
+</div>
+<div class="col-lg-6">
+  <div class="row">
+    <div class="col-12">
+    <h2 class="display-6 mb-5" data-aos="fade-down">There are some important numbers for us. They are just numbers without any meaning, but we just love them.</h2>
+    </div>
+    <div class="col-lg-6" data-aos="fade-up">
+      <div class="display-1 fw-bold py-4">42%</div>
+      <p class="text-black-50">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+    </div>
+    <div class="col-lg-6" data-aos="fade-up">
+      <div class="display-1 fw-bold py-4">+300</div>
+      <p class="text-black-50">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+  </div>
+</div>
+
+<div class="position-relative overflow-hidden w-100 bg-light" id="gallery">
+  <div class="container-fluid">
+  <div class="row overflow-scroll">
+    <div class="col-12">
+      <div class="row vw-100 px-0 py-vh-5 d-flex align-items-center scrollx">
+        <div class="col-md-2" data-aos="fade-up">
+            <img src="img/interior37.jpg" class="rounded shadow img-fluid" alt="nice gallery image" width="512" height="341">
+        </div>
+
+        <div class="col-md-2" data-aos="fade-up" data-aos-delay="200">
+            <img src="img/people1.jpg" class="img-fluid rounded shadow" alt="nice gallery image" width="1164" height="776">
+        </div>
+
+        <div class="col-md-3" data-aos="fade-up" data-aos-delay="400">
+            <img src="img/people2.jpg" class="img-fluid rounded shadow" alt="nice gallery image" width="844" height="1054">
+        </div>
+
+        <div class="col-md-3" data-aos="fade-up" data-aos-delay="600">
+            <img src="img/interior29.jpg" class="img-fluid rounded shadow" alt="nice gallery image" width="844" height="562">
+        </div>
+
+        <div class="col-md-2" data-aos="fade-up" data-aos-delay="800">
+            <img src="img/people23.jpg" class="rounded shadow img-fluid" alt="nice gallery image" width="512" height="341">
+        </div>
+      </div>
+    </div>
+   </div>
+ </div>
+
+</div>
+
+<div class="container py-vh-4 w-100 overflow-hidden">
+  <div class="row d-flex justify-content-center align-items-center">
+    <div class="col-lg-5">
+      <h3 class="py-5 border-top border-dark" data-aos="fade-right">關於我們</h3>
+    </div>
+    <div class="col-md-7" data-aos="fade-left">
+      <blockquote>
+        <div class="fs-4 my-3 fw-light pt-4 border-bottom pb-3">本公司（金勾盃股份有限公司）為皇盃國際塑膠廠的總代理商，
+雖然本公司上市未達１年，但本公司保持著以客至上、客為優先的態度在營運，所以客戶可以完全的放心交給我們！
+我們的塑膠環保杯為前所未見得新開發產品，
+專利擁有者在２０２１年的某一天，在等紅燈的途中，看了一下飲料店擁有美貌的飲料店服務員，正在欣賞的途中，突然看到了正在買飲料的客人買了袋子準備裝飲料，頓時腦海中浮出了「為何不在飲料杯上多做一個提環，店家、客人都方便且還環保！！」於是專利擁有者開始動身，經過了許多挫折、壓力、測試、意見不合等，
+於是，燈愣～「金勾杯」就誕生了。
+        </div>
+    <!--<img src="img/person11.jpg" width="64" height="64" class="img-fluid rounded-circle me-3" alt="" data-aos="fade">
+    <span><span class="fw-bold">John Doe,</span>
+      CEO of Stride Ltd.</span>-->
+      </blockquote>
+    </div>
+  </div>
+</div>
+
+<div class="py-vh-2 bg-gray-900 text-light w-100 overflow-hidden" id="order">
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="row d-flex justify-content-center text-center">
+        <div class="col-lg-8 text-center" data-aos="fade">
+          <div class="row">
+            <div class="col center">
+              <form class="form-floating">
+              <div class="col">
+                <h3 class="h5 my-2" data-aos="fade-left">請輸入您的大名</h3>
+                <input data-aos="fade-left" type="text" class="form-control p-3" id="name" placeholder="請輸入姓名">
+              </div>
+              <br>
+              <div class = "col">
+              <h3 class="h5 my-2" data-aos="fade-left">請輸入您的地址</h3>
+                <input data-aos="fade-right" type="text" class="form-control p-3" id="address" placeholder="請輸入地址">
+              </div>
+              <br>
+              <div class = 'col'>
+              <h3 class="h5 my-2" data-aos="fade-left">請輸入您的電話</h3>
+                <input data-aos="fade-left" type="text" class="form-control p-3" id="phone" placeholder="09XXXXXXXX">
+              </div>
+              <br>
+              <div class = 'col'>
+              <h3 class="h5 my-2" data-aos="fade-left">請輸入欲訂購的尺寸</h3>
+              <select data-aos="fade-right" class = "form-control p-3" id = "size" placeholder = "請選擇尺寸">
+                <option value = 700>PP700</option>
+                <option value = 660>PP660</option>
+                <option value = 500>PP500</option>
+              </select>
+              </div>
+              <br>
+              <div class = 'col'>
+              <h3 class="h5 my-2" data-aos="fade-left">請輸入欲訂購的箱數(1000杯/箱)</h3>
+                <input data-aos="fade-left" type="number" class="form-control p-3" id="amount" placeholder="箱數">
+              </div>
+              <br>
+              <div class = 'col'>
+              <h3 class="h5 my-2" data-aos="fade-left">請選擇業務員名稱</h3>
+              <select data-aos="fade-right" class = "form-control p-3" id = "seller" placeholder = "請選擇業務姓名">
+                <option value = 0>業務一</option>
+                <option value = 1>業務二</option>
+              </select>
+              </div>
+              <div class="col-12">
+                <a href="#" class="btn btn-warning btn-xl shadow me-3 mt-4" data-aos="fade-up">訂購</a>
+              </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!--<div class="small py-vh-3 w-100 overflow-hidden">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-lg-4 border-end" data-aos="fade-up">
+        <div class="d-flex">
+          <div class="col-md-3 flex-fill pt-3 pe-3 pe-md-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi bi-box-seam" viewbox="0 0 16 16">
+              <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+            </svg>
+          </div>
+          <div class="col-md-9 flex-fill">
+            <h3 class="h5 my-2">功能介紹</h3>
+            <p>功能內容</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 border-end" data-aos="fade-up" data-aos-delay="200">
+        <div class="d-flex">
+          <div class="col-md-3 flex-fill pt-3 pt-3 pe-3 pe-md-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi bi-card-checklist" viewbox="0 0 16 16">
+              <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+              <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
+            </svg>
+          </div>
+          <div class="col-md-9 flex-fill">
+          <h3 class="h5 my-2">功能介紹</h3>
+            <p>功能內容</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="400">
+        <div class="d-flex">
+          <div class="col-md-3 flex-fill pt-3 pt-3 pe-3 pe-md-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi bi-window-sidebar" viewbox="0 0 16 16">
+              <path d="M2.5 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+              <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v2H1V3a1 1 0 0 1 1-1h12zM1 13V6h4v8H2a1 1 0 0 1-1-1zm5 1V6h9v7a1 1 0 0 1-1 1H6z"/>
+            </svg>
+          </div>
+          <div class="col-md-9 flex-fill">
+            <h3 class="h5 my-2">功能介紹</h3>
+            <p>功能內容</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>-->
+</main>
+
+<footer>
+<div class="container small border-top">
+  <div class="row py-5 d-flex justify-content-between">
+    <div class="col-12 text-center">
+      <?php
+        include "footer.php"
+      ?>
+    </div>
+  </div>
+</div>
+</footer>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/aos.js"></script>
+ <script>
+ AOS.init({
+   duration: 800, // values from 0 to 3000, with step 50ms
+ });
+ </script>
+
+ <script>
+  let scrollpos = window.scrollY
+  const header = document.querySelector(".navbar")
+  const header_height = header.offsetHeight
+
+  const add_class_on_scroll = () => header.classList.add("scrolled", "shadow-sm")
+  const remove_class_on_scroll = () => header.classList.remove("scrolled", "shadow-sm")
+
+  window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= header_height) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+
+    console.log(scrollpos)
+  })
+</script>
+
+  </body>
+</html>
