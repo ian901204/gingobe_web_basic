bde48c7a389e18720dbd852483745a698fb2483e
ian901204 <scckate444@gmail.com>
2022-09-20 16:12:30 +0800

    update

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -1,7 +1,6 @@
 <?php
 	namespace App\Controllers;
 
-	use App\Middleware\corsMiddleware;
 	use Psr\Http\Message\ResponseInterface;
 	use Psr\Http\Message\ServerRequestInterface;
 	use App\Models\DB\Order;
