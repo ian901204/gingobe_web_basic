fe4851accd1d96b9a688a76852d00ffb88c7c223
ian901204 <scckate444@gmail.com>
2022-09-23 10:18:41 +0800

    update

--- src/Models/DB/Order.php
+++ src/Models/DB/Order.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 use Ramsey\Uuid\Uuid;
 
 class Order extends Model{
-    use HasUuids;
     protected $table = 'orders';
     protected $primaryKey = 'id';
     protected $fillable = ["client_name", "client_phone", "order_address", "seller_id", "product_amount", "product_size", "description"];
