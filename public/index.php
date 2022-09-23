39f91c45aa25895de913f6fd5701981340c47a42
ian901204 <scckate444@gmail.com>
2022-09-23 10:57:06 +0800

    TEST

--- public/index.php
+++ public/index.php
@@ -345,7 +345,7 @@
 
   dataJSON["seller"] = $("#seller").val();
   $("#sendOrder").click(function(e){
-    alert($("#name").val());
+    alert(dataJSON);
     $.ajax({
       url: "https://admin.ian-shen.live/order/add",
       data: JSON.stringify(dataJSON),
