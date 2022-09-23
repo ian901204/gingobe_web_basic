4024002df2ceb31f4bba9723e98c55de284de1ae
ian901204 <scckate444@gmail.com>
2022-09-23 10:42:48 +0800

    update

--- src/Models/DB/Order.php
+++ src/Models/DB/Order.php
@@ -1,24 +1,11 @@
 <?php
 namespace App\Models\DB;
 use Illuminate\Database\Eloquent\Model;
-use Ramsey\Uuid\Uuid;
 
 class Order extends Model{
     protected $table = 'orders';
     protected $primaryKey = 'id';
     protected $fillable = ["client_name", "client_phone", "order_address", "seller_id", "product_amount", "product_size", "description"];
     public $timestamps = false;
-
-    protected $keyType = 'string';
-
-    public $incrementing = false;
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::creating(function (Model $model) {
-            $model->setAttribute($model->getKeyName(), Uuid::uuid4());
-        });
-    }
 }
 ?>
