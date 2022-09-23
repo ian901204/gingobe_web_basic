a7618c920021785a5b481ed98fe9a82007ebdf3a
scckate444 <scckate444@gmail.com>
2022-09-23 19:08:49 +0800

    update

--- backend/menu.php
+++ backend/menu.php
@@ -7,7 +7,7 @@
             <a href="/order/list"><i class="menu-icon ti-email"></i>訂單列表</a>
         </li>
         <li>
-            <a href="/seller/list"><i class="fa-suitcase"></i>業務列表</a>
+            <a href="/seller/list"><i class="ti-shopping-cart-full"></i>業務列表</a>
         </li>
         <li>
             <a href="/frontend/"><i class="fa-file-text-o"></i>前台管理</a>
