89306f0dc82d42d8a727b458dd039ce21b825cd1
ian901204 <scckate444@gmail.com>
2022-09-23 15:26:55 +0800

    update

--- public/index.php
+++ public/index.php
@@ -350,6 +350,7 @@
         $("#warrningText").text($("#" + index).attr('placeholder') + "資料缺少 請確認完整後在送出！");
         $("#warrningText").css("color", 'red');
         data_missing = 1;
+        return false;
       }
     });
     if (data_missing == 0){
