ec1119032b533f38b5e74f15300fd6094fd7b6ac
scckate444 <scckate444@gmail.com>
2022-09-23 18:12:48 +0800

    update

--- public/index.php
+++ public/index.php
@@ -347,7 +347,7 @@
     dataJSON["amount"] = $("#amount").val();
     dataJSON["seller"] = $("#seller").val();
     $.each(dataJSON, function(index, value){
-      if ((value == "") || (value < 0)){
+      if ((value == "") || (value == -1)){
         console.log(index + ":" + value);
         $("#warrningText").text($("#" + index).attr('placeholder') + "資料缺少 請確認完整後在送出！");
         $("#warrningText").css("color", 'red');
