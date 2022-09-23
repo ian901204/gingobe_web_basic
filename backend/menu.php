466c8b8ba15cc0d6a2b6b2b9f8111dd3ec2788c7
scckate444 <scckate444@gmail.com>
2022-09-23 19:11:45 +0800

    feat

--- backend/menu.php
+++ backend/menu.php
@@ -1,16 +1,16 @@
 <div id="main-menu" class="main-menu collapse navbar-collapse">
     <ul class="nav navbar-nav">
         <li>
-            <a href="/"><i class="menu-icon ti-email"></i>總攬</a>
+            <a href="/"><i class="menu-icon ti-image"></i>總攬</a>
         </li>
         <li>
-            <a href="/order/list"><i class="menu-icon ti-email"></i>訂單列表</a>
+            <a href="/order/list"><i class="menu-icon ti-shopping-cart-full"></i>訂單列表</a>
         </li>
         <li>
-            <a href="/seller/list"><i class="menu-icon ti-shopping-cart-full"></i>業務列表</a>
+            <a href="/seller/list"><i class="menu-icon ti-id-badge"></i>業務列表</a>
         </li>
         <li>
-            <a href="/frontend/"><i class="menu-icon fa-file-text-o"></i>前台管理</a>
+            <a href="/frontend/"><i class="menu-icon ti-layout"></i>前台管理</a>
         </li>
     </ul>
 </div><!-- /.navbar-collapse -->
