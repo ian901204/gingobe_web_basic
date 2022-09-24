f2e6a98a91b159ca35aa92e6a46577e174a7fd6f
scckate444 <scckate444@gmail.com>
2022-09-24 17:04:44 +0800

    update

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -26,10 +26,10 @@
 		public function list(ServerRequestInterface $request, ResponseInterface $response){
 			$order = Order::get();
 			$order_data = [];
-			echo $order;
 			foreach($order as $data){
 				array_push($order_data, ["order_id" => $order_data -> getKey(), "client_name" => $data -> client_name, "product_size" => $data -> product_size, "product_amount" => $data -> product_amount]);
 			}
+			echo $order_data;
 			$response -> getBody() -> write(include __DIR__."/../backend/order.php");
 			return $response -> withStatus(200);
 		}
