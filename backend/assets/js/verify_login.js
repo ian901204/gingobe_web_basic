387d766089d5f39d4349c3ad7a756bc02c1177b2
scckate444 <scckate444@gmail.com>
2022-09-24 22:30:29 +0800

    js function update

--- backend/assets/js/verify_login.js
+++ backend/assets/js/verify_login.js
@@ -1,13 +1,16 @@
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
-function verify_token(){
+function verify_token(url){
     $.ajax({
         url: "https://admin.ian-shen.live/verify",
         headers: {"Authorization":"Bearer " + localStorage.getItem('token')},
         type: "POST",
         dataType: "json",
         contentType: "application/json;charset=utf-8",
+        success: function(returnData){
+            window.location.replace(url);
+        },
         error: function(xhr, ajaxOptions, thrownError){
             localStorage.removeItem('token');
             alert("請重新登入!");
