e323c83b58fb94ede87bb62148981e00cfc6aa67
scckate444 <scckate444@gmail.com>
2022-09-25 13:27:00 +0800

    update

--- backend/assets/js/verify_login.js
+++ backend/assets/js/verify_login.js
@@ -1,28 +1,15 @@
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
-var pathname = $(location).attr('pathname');;  
+var pathname = $(location).attr('pathname');
+
 if (pathname != "/login"){
-    $.ajax({
-        url: "https://admin.ian-shen.live/verify",
-        headers: {"Authorization":"Bearer " + localStorage.getItem('token')},
-        type: "POST",
-        dataType: "json",
-        contentType: "application/json;charset=utf-8",
-        success: function(returnData){
-            
-        },
-        error: function(xhr, ajaxOptions, thrownError){
-            localStorage.removeItem('token');
-            alert("請重新登入!" + pathname);
-            window.location.replace("https://admin.ian-shen.live/login");
-        }
-    });
+    verify_token($(location).attr('href'));
 }
 function verify_token(url){
     console.log(localStorage.getItem('token'));
     $.ajax({
-        url: "https://admin.ian-shen.live/verify",
+        url: $(location).attr('origin') + "/verify",
         headers: {"Authorization":"Bearer " + localStorage.getItem('token')},
         type: "POST",
         dataType: "json",
@@ -33,7 +20,7 @@ function verify_token(url){
         error: function(xhr, ajaxOptions, thrownError){
             localStorage.removeItem('token');
             alert("請重新登入!");
-            window.location.replace("https://admin.ian-shen.live/login");
+            window.location.replace($(location).attr('origin') + "/login");
         }
     });
 }
