877b659adedc8707bdf68289da4a9debc62094bc
scckate444 <scckate444@gmail.com>
2022-09-25 14:41:10 +0800

    fix: authMiddleware error

--- src/Middlewares/authMiddleware.php
+++ src/Middlewares/authMiddleware.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\ServerRequestInterface ;
 use Psr\Http\Server\RequestHandlerInterface ;
 use Slim\Psr7\Response;
 
-class verifyMiddleware
+class authMiddleware
 {
     public function __invoke(ServerRequestInterface $request, RequestHandlerInterface $handler)
     {
