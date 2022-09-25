ffb44a133dab694ae62bccb248b1870884bf82c6
scckate444 <scckate444@gmail.com>
2022-09-25 22:42:20 +0800

    update

---
+++ backend/order_detail.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <?php
+        include __DIR__."/head.php";
+    ?>
+</head>
+
+<body>
+    <!-- Left Panel -->
+    <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+            <?php
+                include __DIR__."/menu.php";
+            ?>
+        </nav>
+    </aside>
+    <!-- /#left-panel -->
+    <!-- Right Panel -->
+    <div id="right-panel" class="right-panel">
+        <!-- Header-->
+        <header id="header" class="header">
+            <?php
+                include __DIR__."/header.php";
+            ?>
+        </header>
+        <!-- /#header -->
+        <!-- Content -->
+        <div class="content">
+        <div class="card">
+                            <div class="card-header">
+                                <strong class="card-title">訂單列表</strong>
+                            </div>
+                            <div class="table-stats order-table ov-h">
+                                <table class="table ">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Product Size</th>
+                                            <th>Product Amount</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            foreach($order_data as $data){
+                                                echo "<tr>";
+                                                echo "<td>#".$data["id"]."</td>";
+                                                echo "<td> <span class='name'>".$data["client_name"]."</td>";
+                                                echo "<td> <span class='product'>".$data["product_size"]."</td>";
+                                                echo "<td> <span class='count'>".$data["product_amount"]."</td>";
+                                                echo "<td>";
+                                                echo "<a href = '/order/get/" . $data["id"]. "' class = 'btn btn-success'>顯示</a>";
+                                                echo "<a onclick = 'delete_order(".$data["id"].")' value = ".$data["id"]." class = 'btn btn-danger'>刪除</a>";
+                                                echo "</td>";
+                                                echo "</tr>";
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div> <!-- /.table-stats -->
+                        </div>
+        </div>
+        <!-- /.content -->
+        <div class="clearfix"></div>
+        <!-- Footer -->
+        <?php
+            include "footer.php";
+        ?>
+        <!-- /.site-footer -->
+    </div>
+    <!-- /#right-panel -->
+
+    <!-- Scripts -->
+    <script>
+    </script>
+    <?php
+        include "buttom_script.php";
+    ?>
+</body>
+</html>
