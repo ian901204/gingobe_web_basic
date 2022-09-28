6824cdf80913c3e5d8b281f5e576123e61078951
ian901204 <scckate444@gmail.com>
2022-09-28 21:52:35 +0800

    update

--- src/Models/DB/admin.php
+++ src/Models/DB/admin.php
@@ -5,7 +5,8 @@ use Illuminate\Database\Eloquent\Model;
 class admin extends Model{
     protected $table = 'admins';
     protected $primaryKey = 'id';
-    protected $fillable = ["name", "account", "password"];
+    protected $guarded = ["id", "password"];
+    protected $hidden = ["id", "password"];
     public $timestamps = false;
     public function check_password($password){
         if ($this -> password == $password){
