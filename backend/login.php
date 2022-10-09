70f11df2f168baed1b71b584afb058b74825c5c3
ian901204 <scckate444@gmail.com>
2022-10-09 17:26:43 +0800

    update

--- backend/login.php
+++ backend/login.php
@@ -28,7 +28,7 @@
                             <label>Password</label>
                             <input id = "password" type="password" class="form-control" placeholder="Password">
                         </div>
-                        <button onclick = "login()" class="btn btn-success btn-flat m-b-30 m-t-30">Sign in</button>
+                        <a onclick = "login()" class="btn btn-success btn-flat m-b-30 m-t-30">Sign in</a>
                     </form>
                 </div>
             </div>
