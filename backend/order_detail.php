299d03e66856722264869b35d01dc740c9e9c69b
ian901204 <scckate444@gmail.com>
2022-10-12 01:07:32 +0800

    update

--- backend/order_detail.php
+++ backend/order_detail.php
@@ -77,7 +77,7 @@
                                 <div class="input-group">
                                 <div class="input-group-addon"><i class="fa fa-user"></i></div>
                                     <select name="select" id="select" class="form-control">
-                                        <option value="0">請選擇業務員</option>
+                                        <option value="-1">請選擇業務員</option>
                                         <?php   
                                             foreach($seller_data as $data){
                                                 if ($data["id"] == $order_data["seller_id"]){
