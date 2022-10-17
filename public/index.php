bd3afb5d8cc5e608f234859e70f4752ef68ac2ae
ian901204 <scckate444@gmail.com>
2022-10-17 19:15:11 +0800

    update

--- public/index.php
+++ public/index.php
@@ -387,7 +387,7 @@
     contentType: "application/json;charset=utf-8",
     dataType:"json",
     success: function(select_data){
-      $.each(select_data, function(index, value){
+      $.each(select_data, function(index, data){
         $('#size').append($('<option>', {value: data["id"],text: data["size"]}));
       });
   }
