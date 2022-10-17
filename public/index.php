057fc1595dd1992c6269426979271a5a959cfcce
ian901204 <scckate444@gmail.com>
2022-10-17 18:57:17 +0800

    update

--- public/index.php
+++ public/index.php
@@ -389,7 +389,7 @@
     type: "POST",
     contentType: "application/json;charset=utf-8",
     success: function(select_data){
-      var set_select = JSON.parse(JSON.stringify(select_data));
+      var set_select = JSON.parse(JSON.stringify(select_data))[0];
       $.each(set_select, function(data){
         $('#size').append($('<option>', {
             value: date["id"],
