2e67c2ab9d72a0cd752531fe358512e23f91167e
ian901204 <scckate444@gmail.com>
2022-10-21 13:07:02 +0800

    update

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -49,6 +49,7 @@
 		public function list(ServerRequestInterface $request, ResponseInterface $response){
 			$order_data = Order::get(["id", "client_name", "product_size", "product_amount", "client_phone", "order_address", "seller_id"]);
 			foreach ($order_data as $data){
+				echo $data["order_address"];
 				if ($data["seller_id"] != -1 || $data["seller_id"] != 0){
 					$seller_data = sellers::where("id", "=", $data["seller_id"])->first();
 					$data["seller_id"] = $seller_data->name;
