66eb3e66dbbb44c11ed03e4fa3f93783eb1aca85
ian901204 <scckate444@gmail.com>
2022-10-22 23:33:28 +0800

    feat: update

--- backend/seller/seller.php
+++ backend/seller/seller.php
@@ -37,8 +37,9 @@
                         <div class="card">
                             <div class="card-header">
                                 <strong class="card-title">訂單列表</strong>
+                                <button class="btn btn-success btn-sm" onclick="add_seller()">新增業務員</button>
                             </div>
-                            <div class="table-stats order-table ov-h">
+                            <div class="card-body">
                                 <table id="bootstrap-data-table" class="table table-striped table-bordered">
                                     <thead>
                                         <tr>
