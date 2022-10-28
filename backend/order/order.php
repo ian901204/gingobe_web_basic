7b24b56787a03f3d5b1e3f8eae4c2d8d7ae2b413
ian901204 <scckate444@gmail.com>
2022-10-28 13:55:37 +0800

    fix: button position

--- backend/order/order.php
+++ backend/order/order.php
@@ -65,10 +65,10 @@
                                                 echo "<td> ".$data["order_address"]."</td>";
                                                 echo "<td> ".$data["seller_id"]."</td>";
                                                 echo "<td> ".$data["order_time"]."</td>";
-                                                echo "<td><div class = 'row'><div class = 'col-lg-6'>";
+                                                echo "<td>";
                                                 echo "<button onclick = 'get_detail(" . $data["id"]. ")' class = 'btn btn-success btn-block'>顯示</button>";
-                                                echo "</div><div class = 'col-lg-6'><button onclick = 'delete_order(".$data["id"].")' value = ".$data["id"]." class = 'btn btn-danger btn-block'>刪除</button>";
-                                                echo "</div></div></td>";
+                                                echo "<button onclick = 'delete_order(".$data["id"].")' value = ".$data["id"]." class = 'btn btn-danger btn-block'>刪除</button>";
+                                                echo "</td>";
                                                 echo "</tr>";
                                             }
                                         ?>
