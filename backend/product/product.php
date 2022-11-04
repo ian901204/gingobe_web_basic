26b85a6a172e22db2925f3b00de8a1f23d6e1a08
ian901204 <scckate444@gmail.com>
2022-11-04 13:51:50 +0800

    fix: edit product with id

--- backend/product/product.php
+++ backend/product/product.php
@@ -54,7 +54,7 @@
                                                 echo "<td> <span class='product'>".$data["price"]."</td>";
                                                 echo "<td></td>";
                                                 echo "<td><div class = 'row'><div class = 'col-4'>";
-                                                echo "<button onclick = 'get_detail(\"" . $data["size"]. "\")' class = 'btn btn-success btn-block'>編輯</button>";
+                                                echo "<button onclick = 'get_detail(\"" . $data["id"]. "\")' class = 'btn btn-success btn-block'>編輯</button>";
                                                 echo "</div><div class = 'col-4'><button onclick = 'product_delete(".$data["id"].",\"" . $data["size"]. "\")' class = 'btn btn-danger btn-block'>刪除</button>";
                                                 echo "</div></div></td>";
                                                 echo "</tr>";
