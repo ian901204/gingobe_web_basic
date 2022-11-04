c9c093f20f1af8ba8443484489008f2de1474b85
ian901204 <scckate444@gmail.com>
2022-11-04 14:23:13 +0800

    update

--- backend/index.php
+++ backend/index.php
@@ -67,7 +67,6 @@ $app -> group("/product", function (RouteCollectorProxy $group){
     $group -> get("/{size}", "App\Controllers\ProductController:get_data");
     $group -> group("/queue", function (RouteCollectorProxy $queue){
         $queue -> get("/list", "App\Controllers\ProductController:queue_list");
-        $queue -> post("/action", "App\Controllers\ProductController:queue") -> add(authMiddleware::class);
         $queue -> post("/down", "App\Controllers\ProductController:queue_down");
         $queue -> post("/up", "App\Controllers\ProductController:queue_up");   
     });
