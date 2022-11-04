59666186e59f55b0a45b6b29e470b227bf53ba73
ian901204 <scckate444@gmail.com>
2022-11-04 14:07:55 +0800

    test: product route testting

--- backend/index.php
+++ backend/index.php
@@ -67,12 +67,11 @@ $app -> group("/product", function (RouteCollectorProxy $group){
 
     $group -> get("/{size}", "App\Controllers\ProductController:get_data");
 
+    $queue -> get("/queue", "App\Controllers\ProductController:queue_list");
+
+    $queue -> post("/queue", "App\Controllers\ProductController:queue")-> add(authMiddleware::class);
+
     $group -> group("/queue", function (RouteCollectorProxy $queue){
-
-        $queue -> get("/", "App\Controllers\ProductController:queue_list");
-
-        $queue -> post("/", "App\Controllers\ProductController:queue")-> add(authMiddleware::class);
-
         $queue -> post("/down", "App\Controllers\ProductController:queue_down");
 
         $queue -> post("/up", "App\Controllers\ProductController:queue_up");
