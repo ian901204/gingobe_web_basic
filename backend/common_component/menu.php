6727cb80ef4718289cd865bf9688ce488f04abc1
ian901204 <scckate444@gmail.com>
2022-12-11 00:37:06 +0800

    fix menu order page error

--- backend/common_component/menu.php
+++ backend/common_component/menu.php
@@ -1,7 +1,7 @@
 <div id="main-menu" class="main-menu collapse navbar-collapse">
     <ul class="nav navbar-nav">
         <li id = "menu_li">
-            <a id = "order_a' href="/order/list"><i class="menu-icon ti-shopping-cart-full"></i>訂單列表</a>
+            <a id = "order_a" href="/order/list"><i class="menu-icon ti-shopping-cart-full"></i>訂單列表</a>
         </li>
         <li  id = "menu_li">
             <a id = "seller_a" href="/seller/list"><i class="menu-icon ti-id-badge"></i>業務列表</a>
