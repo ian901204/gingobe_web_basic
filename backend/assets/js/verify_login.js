5bd703224cf5f7d81f4776af1cc5295683a2b65e
ian901204 <scckate444@gmail.com>
2022-12-11 15:49:29 +0800

    fix

--- backend/assets/js/verify_login.js
+++ backend/assets/js/verify_login.js
@@ -3,7 +3,7 @@ script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 var pathname = $(location).attr('pathname');
 
-if ((pathname != "/login") || (pathname != "/")){
+if ((pathname != "/login") && (pathname != "/")){
     $.ajax({
         url: $(location).attr('origin') + "/verify",
         headers: {"Authorization":"Bearer " + localStorage.getItem('token')},
