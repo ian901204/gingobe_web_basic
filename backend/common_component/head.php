96eef2b42198607ef9101eda85721a1812e288e8
ian901204 <scckate444@gmail.com>
2022-12-11 15:45:26 +0800

    update

--- backend/common_component/head.php
+++ backend/common_component/head.php
@@ -1,25 +1,24 @@
 <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>金勾杯後台</title>
-    <meta name="description" content="Ela Admin - HTML5 Admin Template">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <base href="https://admin.ian-shen.live/" target="_self">
-    <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>金勾杯後台</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<base href="https://admin.ian-shen.live/" target="_self">
+<link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
+<link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
-    <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+<link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
+<link rel="stylesheet" href="assets/css/style.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="assets/js/verify_login.js"></script>
-    <script>
-        $('link[href="assets/css/style.css"]').attr('href',$(location).attr('origin') + '/assets/css/style.css');
-        $('link[href="assets/css/cs-skin-elastic.css"]').attr('href',$(location).attr('origin') + '/assets/css/cs-skin-elastic.css');
-        $('script[src="assets/js/verify_login.js"]').attr('src',$(location).attr('origin') + '/assets/js/verify_login.js');
-    </script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="assets/js/verify_login.js"></script>
+<script>
+    $('link[href="assets/css/style.css"]').attr('href',$(location).attr('origin') + '/assets/css/style.css');
+    $('link[href="assets/css/cs-skin-elastic.css"]').attr('href',$(location).attr('origin') + '/assets/css/cs-skin-elastic.css');
+    $('script[src="assets/js/verify_login.js"]').attr('src',$(location).attr('origin') + '/assets/js/verify_login.js');
+</script> 
