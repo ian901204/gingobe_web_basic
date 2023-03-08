a0217cadd3d648e5d887fdf9f594ec7fed58ceb0
ian901204 <scckate444@gmail.com>
2023-03-08 20:28:15 +0800

    fix: docker-compose

--- dockerfile.php
+++ dockerfile.php
@@ -1,2 +1,23 @@
 FROM php:8.1-fpm
 RUN docker-php-ext-install pdo_mysql
+
+RUN apt-get update && \
+    apt-get install -y \
+        libicu-dev \
+        libpq-dev \
+        git && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN docker-php-ext-install \
+        intl \
+        pdo \
+        pdo_pgsql \
+        pgsql
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+WORKDIR /code
+
+COPY ./web .
+
+RUN composer install --no-dev --no-interaction --no-progress
